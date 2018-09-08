40d9f604a25f46e8c90d622327d31dd6513266f2
Edgar <Edgar@AnotherFoxGuy.com>
2018-09-08 11:00:11 +0200

    Use collect_libs for finding libs

--- conanfile.py
+++ conanfile.py
@@ -41,4 +41,9 @@ class OgredepsConan(ConanFile):
         cmake.install()
 
     def package_info(self):
-        self.cpp_info.libs = ["OGRE-deps"]
+        self.cpp_info.libdirs = ['lib', 'lib/release', 'lib/debug']	 # Directories where libraries can be found
+        self.cpp_info.libs = tools.collect_libs(self)
+        if self.settings.build_type == "Release":
+            self.cpp_info.libs.extend(tools.collect_libs(self, folder="lib/release"))
+        else:
+            self.cpp_info.libs.extend(tools.collect_libs(self, folder="lib/debug"))
